0eb8f0ab15549d3dad2f5c1149cea8e04f406d0c
Hudson Daniel <hudsonprem97@gmail.com>
2020-10-12 17:57:42 +0530

    Updates

---
+++ precache-manifest.9df9b381c8fe9f549b82539dfc14d82c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87acc4182653178e6618f6a1754ad40",
+    "url": "/kssmartask/index.html"
+  },
+  {
+    "revision": "43e606bc2129001b9488",
+    "url": "/kssmartask/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "c3e85d09ca3331024510",
+    "url": "/kssmartask/static/css/main.182f9a6d.chunk.css"
+  },
+  {
+    "revision": "43e606bc2129001b9488",
+    "url": "/kssmartask/static/js/2.467900f9.chunk.js"
+  },
+  {
+    "revision": "c1e665248f04cd3849d99073e416e494",
+    "url": "/kssmartask/static/js/2.467900f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e85d09ca3331024510",
+    "url": "/kssmartask/static/js/main.8cb86cc6.chunk.js"
+  },
+  {
+    "revision": "bdf6dd0abce8225b783d",
+    "url": "/kssmartask/static/js/runtime-main.a46f22eb.js"
+  }
+]);
